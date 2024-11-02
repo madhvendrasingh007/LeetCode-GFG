6286e03de69791d02bd895a20066415fbd515b32
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-11-02 21:39:09 +0530

    Time: 0 ms (100%), Space: 7.5 MB (72.51%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,12 @@
+// Dhairya Kumar Singh
+
+class Solution 
+{
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1;
+    }
+};
