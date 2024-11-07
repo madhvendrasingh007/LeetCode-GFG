4b0f4430894aa624dc62afef56cc314d06db1f8b
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-11-07 22:12:26 +0530

    Time: 54 ms (28.55%), Space: 102 MB (84.89%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,31 @@
+class RandomizedSet {
+public:
+    
+    vector<int> nums;
+    unordered_map<int, int> m;
+    
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
