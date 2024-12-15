22cb6ba0c26c0daa2ac02d865af3ffec910646bc
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-12-15 17:08:37 +0530

    Time: 5 ms (99.33%), Space: 79.5 MB (83.14%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    vector<int> findNse(vector<int> arr, int n){
-        stack<int> st;
-        vector<int> nse(n, -1);
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
-                st.pop();
-            }
-            nse[i] = st.empty()?n:st.top();
-            st.push(i);
-        }
-        return nse;
-    }
-    vector<int> findPse(vector<int> arr, int n){
-        stack<int> st;
-        vector<int> pse(n, -1);
-        for(int i=0; i<n; i++){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
-                st.pop();
-            }
-            pse[i] = st.empty()?-1:st.top();
-            st.push(i);
-        }
-        return pse;
-    }
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> nse = findNse(heights, n);
-        vector<int> pse = findPse(heights, n);
-        int maxi = 0;
-        for(int i=0; i<n; i++){
-            maxi = max(maxi, (heights[i]*(nse[i]-pse[i]-1)));
+        stack<int> st;
+        int maxA=0,n=heights.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && ( i==n || heights[st.top()]>=heights[i])){
+                // picking up the previous rectange bar
+                int currHeight=heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()){
+                    width=i;
+                }
+                else{
+                    // as we are storing indexes in stack and those are needed for finding the width
+                    width = i-st.top()-1;
+                }
+                maxA= max(maxA, currHeight*width);
+            }
+            st.push(i);
         }
-        return maxi;
+        return maxA;
     }
 };
