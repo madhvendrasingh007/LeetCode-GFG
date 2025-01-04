e01147b3ba0875aab961aa7b4f2ea7d89341e5e6
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-01-04 20:17:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (76.23%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long int ans = 0;
-        int i = 0;
-        
-        // Skip leading whitespaces
-        while(s[i] == ' ') i++;
-        
-        int sign = 0;   // 0 -> + , 1 -> -
-        
-        // Handle signs
-        if(s[i] == '-') { 
-            sign = 1;
-            i++;
-        } else if(s[i] == '+') {
-            sign = 0;
+        int len = s.size();
+        double num = 0;
+        int i=0;
+        while(s[i] == ' '){
             i++;
         }
-        
-        // Skip leading zeros
-        while(s[i] == '0') i++;
-        
-        // Convert characters to integer
-        while(s[i] >= '0' && s[i] <= '9') {
-            if(ans > INT_MAX) {
-                if(sign) return INT_MIN;
-                return INT_MAX;
-            }
-            ans = ans * 10 + s[i] - '0';
+        bool positive = s[i] == '+';
+        bool negative = s[i] == '-';
+        positive == true ? i++ : i;
+        negative == true ? i++ : i;
+        while(i < len && s[i] >= '0' && s[i] <= '9'){
+            num = num*10 + (s[i]-'0');
             i++;
         }
-        
-        // Apply sign
-        if(sign) {
-            ans *= -1;
-        }
-        
-        // Handle overflow/underflow
-        if(ans > INT_MAX) return INT_MAX;
-        if(ans < INT_MIN) return INT_MIN;
-        
-        return (int) ans;
+        num = negative ? -num : num;
+        num = (num > INT_MAX) ? INT_MAX : num;
+        num = (num < INT_MIN) ? INT_MIN : num;
+        return int(num);
     }
 };
