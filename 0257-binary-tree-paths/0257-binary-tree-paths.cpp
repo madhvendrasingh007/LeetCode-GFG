eb78862ff122f1d3ed8e7c12dd6a3bd8dfb0b8a9
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-01-31 23:45:16 +0530

    Time: 0 ms (100%), Space: 18.3 MB (18.73%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,19 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void helper(TreeNode* root,vector<string>& answer,string curr){
-        if(!root)return;
-        if(root->left||root->right)curr+=(to_string(root->val)+"->");
-        else{
-            curr+=(to_string(root->val));
-            answer.push_back(curr);
+vector<string>v;
+    
+    void solve(TreeNode* root,string s){
+        if(!root){
+            if(s!="")
+            return;
+            else
+            v.push_back(s);
+            return;
         }
-        helper(root->left,answer,curr);
-        helper(root->right,answer,curr);  
-    }
+
+        s=s+to_string(root->val)+"->";
+        if(!root->left and !root->right){
+            s=s.substr(0,s.size()-2);
+            v.push_back(s);
+        }
+        if(root->left)
+        solve(root->left,s);
+        if(root->right)
+        solve(root->right,s);
+        
+       
+        
+}
+
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> answer;
-        string curr="";
-        helper(root,answer,curr);
-        return answer;
+        string s="";
+        solve(root,s);
+        return v;
+        
     }
 };
