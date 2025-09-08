c67894b8c74a62d9381bd635519e925ad3616490
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-09-08 22:17:29 +0530

    Time: 0 ms (100%), Space: 11.3 MB (45.51%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int breaks = 0;
-        int n = nums.size();
-        
-        // Count adjacent breaks
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) breaks++;
-        }
-        
-        // Count circular break
-        if (nums[n - 1] > nums[0]) breaks++;
-        
-        return breaks <= 1;
+        int countDrop = 0, n = nums.size();
+        for (int i = 1; i < n; ++i)
+            if (nums[i] < nums[i - 1])
+                ++countDrop;
+        if (nums[0] < nums[n - 1])
+            ++countDrop;
+        return countDrop <= 1;
     }
 };
